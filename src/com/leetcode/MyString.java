2ece7b385ad283e9704039e687fd301f7bd2100d
qizhang1 <qi.zhang04@sap.com>
2020-08-08 23:27:43 -0700

    LC-383. Ransom Note

--- src/com/leetcode/MyString.java
+++ src/com/leetcode/MyString.java
@@ -703,6 +703,24 @@ public class MyString {
         return -1;
     }
   
+    // LC-383. Ransom Note
+    // Time O(n+m), Space O(1)
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int[] count = new int[26];
+        for (int i = 0; i < magazine.length(); i++) {
+            char c = magazine.charAt(i);
+            count[c-'a']++;
+        }
+        
+        for (int i = 0; i < ransomNote.length(); i++) {
+            char c = ransomNote.charAt(i);
+            count[c-'a']--;
+            if (count[c-'a'] < 0) {
+                return false;
+            }
+        }
+        return true;
+    }
     
 	
 	
