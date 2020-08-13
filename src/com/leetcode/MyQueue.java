40793e517d4b91eb1a9352645503d32482fdcdf3
qizhang1 <qi.zhang04@sap.com>
2020-08-12 22:17:29 -0700

    LC-232. Implement Queue using Stacks

--- src/com/leetcode/MyQueue.java
+++ src/com/leetcode/MyQueue.java
@@ -3,45 +3,43 @@ package com.leetcode;
 import java.util.Stack;
 
 public class MyQueue {
-	/* Java Queue Interface 
-	*  offer -- return true or false  
-	*  poll, peek -- return null if this queue is empty
-	*  add -- returning true upon success and throws IllegalStateException if no space
-	*  remove, element -- throws NoSuchElementException if this queue is empty */
-	
-	// ****************************************************************************************
-	// Implement Queue using Stacks 
-	class Queue {
-	    Stack<Integer> in = new Stack<>();
-	    Stack<Integer> out = new Stack<>();
-	    
-	    public void enqueue(int value) {
-	    	in.push(value);
-	    }
-	    
-	    public Integer dequeue() { 
-//	        if (out.empty()){
-//	            while (!in.empty()){
-//	                out.push(in.pop());
-//	            }
-//	        }
-	    	peek(); // use implementation in peek()
-	        return out.pop();
-	    } 
-	    
-	    public int peek() {
-	    	if (out.empty()){
-	    		while (!in.empty()){
-	    			out.push(in.pop());
-	    		}
-	    	}
-	        return out.peek();
-	    }
 
+	// LC-232. Implement Queue using Stacks 
+	class Queue {
+		private Stack<Integer> s1 = new Stack<>();
+		private Stack<Integer> s2 = new Stack<>(); 
+	    private int front;
+	    
+	    // Time O(1)
+	    public void push(int x) {
+	        if (s1.empty()) {
+	            front = x;
+	        }
+	        s1.push(x);
+	    }
+	    
+	    // Time Amortized O(1), Worst-case O(n)
+	    public int pop() {
+	    	// push all elements of s1 to stack s2, which helps to store the elements in reversed order
+	        if (s2.isEmpty()) {
+	            while (!s1.isEmpty()) {
+	                s2.push(s1.pop());
+	            }
+	        }
+	        return s2.pop(); 
+	    }
+	    
+	    // Time O(1)
+	    public int peek() {
+	        if (!s2.isEmpty()) {
+	           return s2.peek();
+	        }
+	        return front;
+	    }
+	    
+	    // Time O(1)
 	    public boolean empty() {
-	        return in.isEmpty() && out.isEmpty();
+	        return s1.isEmpty() && s2.isEmpty();
 	    }
 	}
-	
-	
 }
