76623b135eddc7f6af06b378528905cbeb74cb98
qizhang1 <qi.zhang04@sap.com>
2020-08-29 10:39:01 -0700

    LC-112 path sum

--- src/com/leetcode/MyBinaryTree.java
+++ src/com/leetcode/MyBinaryTree.java
@@ -705,8 +705,7 @@ public class MyBinaryTree {
 	// *****************************************************************************************
 	// expression tree
 
-	// *****************************************************************************************
-	// "root-to-leaf path" problem
+	// LC-112. "root-to-leaf" Path Sum
 	// Return true if the tree has a root-to-leaf path such that adding up all
 	// the values along the path equals the given sum. Otherwise, Return false.
 	public static boolean hasPathSum(TreeNode root, int sum) {
@@ -716,8 +715,7 @@ public class MyBinaryTree {
 		if (root.left == null && root.right == null) {
 			return sum == root.val;
 		}
-		return hasPathSum(root.left, sum - root.val)
-				|| hasPathSum(root.right, sum - root.val);
+		return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val);
 	}
 
 	// *****************************************************************************************
